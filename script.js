5e902fc0bb3d0303af4c534beb701049c9feb03e
Dominic Reichl <office@dominicreichl.com>
2024-09-19 19:05:01 +0200

    scroll to top when loading page

--- script.js
+++ script.js
@@ -10,6 +10,7 @@ function loadPage(pageUrl) {
     xhr.onreadystatechange = function () {
         if (xhr.readyState === 4 && xhr.status === 200) {
             document.getElementById('content').innerHTML = xhr.responseText;
+            window.scrollTo({ top: 0, behavior: 'smooth' });
         }
     };
     xhr.send();
