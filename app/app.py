cf6a904ac632e5dc3b730c0c961193ce8c1f4691
Dominic Reichl <office@dominicreichl.com>
2024-09-28 11:10:06 +0200

    finish the llm app

--- app/app.py
+++ app/app.py
@@ -1,7 +1,44 @@
-import os, sys
+import os, string, sys
 from flask import Flask, jsonify, request
+from haystack.components.generators import HuggingFaceAPIGenerator
+from haystack.utils import Secret
 
 sys.path.insert(0, os.path.dirname(__file__))
+
+
+def get_reply_from_llm(concept: str, token: str) -> str:
+    generator = HuggingFaceAPIGenerator(
+        api_type="serverless_inference_api",
+        api_params={"model": "meta-llama/Meta-Llama-3-8B-Instruct"},
+        token=Secret.from_token(token),
+    )
+    result = generator.run(
+        prompt=f"""Does the following string contain a philosophical concept?
+            ---
+            String: {concept}
+            ---
+            Answer with 'yes' or 'no' only.
+            """,
+        generation_kwargs={"max_new_tokens": 8},
+    )
+    reply = result["replies"][0]
+    concept_is_philosophical = "yes" in reply.lower()
+    if concept_is_philosophical:
+        result = generator.run(
+            prompt=f"Here is a short paragraph of philosophical wisdom in the style of Spinoza on the topic of {concept}: ",
+            generation_kwargs={"max_new_tokens": 100},
+        )
+        reply = (
+            ".".join(result["replies"][0].split(".")[:-1])
+            .replace('"', "")
+            .replace("“", "")
+            .lstrip(string.whitespace)
+            + "."
+        )
+    else:
+        reply = f"'{concept}' is not a philosophical concept."
+    return reply
+
 
 app = Flask(__name__)
 
@@ -14,14 +51,12 @@ def welcome():
 @app.route("/run-python", methods=["POST"])
 def run_python():
     concept = request.json.get("input_value")
-
-    concept_is_valid = concept in ["freedom", "morality"]
-
-    if concept_is_valid:
-        return jsonify({"result": f"You chose '{concept}'"})
-    else:
+    try:
+        reply = get_reply_from_llm(concept, os.environ.get("HF_ACCESS_TOKEN"))
+        return jsonify({"result": reply})
+    except Exception as e:
         return (
-            jsonify({"error": f"Invalid input value: {concept}; {type(concept)}"}),
+            jsonify({"error": f"Unfortunately, the philosopher is currently asleep."}),
             400,
         )
 
